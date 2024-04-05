f581eda74cfaded57dc11b13396eb2a7669708e9
Enceladus-py <dalsunaberat@gmail.com>
2024-04-05 19:13:16 +0300

    add flink kafka listener

---
+++ jobs/eqstream/src/main/java/com/kartaca/eqstream/EarthquakeStreamJob.java
@@ -0,0 +1,32 @@
+package com.kartaca.eqstream;
+
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.common.serialization.SimpleStringSchema;
+import org.apache.flink.connector.kafka.source.KafkaSource;
+import org.apache.flink.connector.kafka.source.enumerator.initializer.OffsetsInitializer;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+public class EarthquakeStreamJob {
+
+    public static void main(String[] args) throws Exception {
+        final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        KafkaSource<String> source = KafkaSource.<String>builder()
+                .setBootstrapServers("kafka:9092")
+                .setTopics("random-eq-topic")
+                .setGroupId("flink")
+                .setStartingOffsets(OffsetsInitializer.earliest())
+                .setValueOnlyDeserializer(new SimpleStringSchema())
+                .build();
+
+        env.fromSource(source, WatermarkStrategy.noWatermarks(), "Kafka Source")
+            .map((MapFunction<String, String>) value -> {
+                System.out.println(value); // TODO try to move this job to backend
+                return value;
+            });
+
+        env.execute("Earthquake Stream Job");
+    }
+
+}
